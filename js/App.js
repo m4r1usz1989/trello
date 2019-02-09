c93b464852d2894036d25eaeb71729558d745b12
Mariusz Domanski <mariusz@domanski.pl>
2019-02-09 19:33:45 +0100

    recontruction of the aplication

---
+++ js/App.js
@@ -0,0 +1,39 @@
+var baseUrl = 'https://cors-anywhere.herokuapp.com/https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 3819,
+  'X-Auth-Token': 'd564b84b3a39e3bbbbd00cde5d5253eb'
+};
+
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
+
+function setupColumns(columns) {
+  columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+    board.addColumn(col);
+    setupCards(col, column.cards);
+  });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+
